e5c71a7352060f3de786688782df3c00ce660490
neha072001 <134941181+neha072001@users.noreply.github.com>
2023-06-07 10:21:47 +0530

    Add files via upload

---
+++ java_programs/src/Animal_main.java
@@ -0,0 +1,22 @@
+package java_programs;
+
+public class Animal_main {
+
+		  public static void main(String[] args) {
+
+			    // create an object of Dog class
+			    Dog d1 = new Dog();
+
+			    d1.makeSound();
+			    d1.eat();
+			    
+			    // create an object of cat class 
+			    cat c1 = new cat();
+
+			    c1.makeSound();
+			    c1.eat();
+			  }
+			}
+	}
+
+}
---
+++ java_programs/src/animal.java
@@ -0,0 +1,26 @@
+package java_programs;
+
+abstract class Animal {
+	  abstract void makeSound();
+
+	  public void eat() {
+	    System.out.println("I can eat.");
+	  }
+	}
+public class animal {
+	class Dog extends Animal {
+
+	  // provide implementation of abstract method
+	  public void makeSound() {
+	    System.out.println("Bark bark");
+	  }
+public class animal {	  
+	 class cat extends Animal {
+
+		  // provide implementation of abstract method
+		  public void makeSound() {
+		    System.out.println("meow meow");
+	}
+
+}
+
